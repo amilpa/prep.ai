00c5e5040c1432265df10bf78adbf0cfdcdad07c
amilpa <amilpa2020@gmail.com>
2024-06-22 19:03:40 +0530

    change name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 right-0 h-[80px] flex justify-center items-center bg-white">
-      <h1 className="text-3xl font-semibold text-gray-500">Zyntrix</h1>
+      <h1 className="text-3xl font-semibold text-gray-500">Prep.ai</h1>
     </div>
   );
 }
